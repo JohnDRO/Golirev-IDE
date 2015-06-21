4bd71b9bd1c4da82ed26eaaa348f20c39f4f7923
John DRO <baptiste.duprat@u-psud.fr>
2015-06-21 17:18:50 +0200

    Updating svg.pan-zoomjs 1.0.1 -> 2.2.0

--- src/svgjs/svg.pan-zoom.js
+++ src/svgjs/svg.pan-zoom.js
@@ -5,18 +5,20 @@
  * A JavaScript library for pan and zoom SVG things.
  * Created with <3 and JavaScript by the jillix developers.
  *
- * svg.pan-zoom.js 1.0.1
+ * svg.pan-zoom.js 2.2.0
  * Licensed under the MIT license.
  * */
 ;(function() {
 
-    var container = null;
-    var markers = null;
+    var container = null
+      , markers = null
+      , mousewheel = "onwheel" in document.createElement("div")
+      ? "wheel"
+      : document.onmousewheel !== undefined
+      ? "mousewheel"
+      : "DOMMouseScroll"
+      ;
 
-    var mousewheel = "onwheel" in document.createElement("div")
-                     ? "wheel" : document.onmousewheel !== undefined
-                     ? "mousewheel"
-                     : "DOMMouseScroll";
     /**
      * panZoom
      * The pan-zoom contructor.
@@ -26,6 +28,7 @@
      * @param {Object|undefined} opt_options An optional object containing the following fields:
      *
      *  - `zoom` (Array): An array of two float values: the minimum and maximum zoom values (default: `undefined`).
+     *  - `zoomSpeed` (Number): The zoom speed (default: `-1`). By changing the sign the zoom is reversed.
      *
      * @return {PanZoom} The PanZoom object containing the following fields:
      *
@@ -39,15 +42,65 @@
         // Selected element
         var self = this;
 
+        /**
+         * setPosition
+         * Sets the graph position programatically.
+         *
+         * @name setPosition
+         * @function
+         * @param {Number} x The relative position to the svg document (on x axis).
+         * @param {Number} y The relative position to the svg document (on y axis).
+         * @param {Number} z The zoom value which will be handled as `scale` internally.
+         * @return {PanZoom} The `PanZoom` instance.
+         */
+        function setPosition(x, y, z) {
+            pz.pan.iPos = pz.pan.fPos;
+            pz.transform = self.transform();
+            pz.transform.x = x;
+            pz.transform.y = y;
+            if (typeof z === "number") {
+                pz.zoom(z);
+            } else {
+                updateMatrix();
+            }
+            return pz;
+        }
+
+        /**
+         * zoom
+         * Zooms in/out the graph programatically.
+         *
+         * @name zoom
+         * @function
+         * @param {Number} z The zoom value which will be handled as `scale` internally.
+         * @return {PanZoom} The `PanZoom` instance.
+         */
+        function zoom(z) {
+            if (typeof z === "number") {
+                pz.transform = self.transform();
+                pz.transform.scaleY = pz.transform.scaleX = z;
+                updateMatrix();
+                return pz;
+            }
+
+            pz.transform = self.transform();
+            pz.transform.scaleY = pz.transform.scaleX = z;
+            updateMatrix();
+            return pz;
+        }
+
         // Pan zoom object
         var pz = {
-            pan: {},
-            elm: self
+            pan: {}
+          , elm: self
+          , setPosition: setPosition
+          , zoom: zoom
         };
 
         // Set options
         opt_options = Object(opt_options);
         opt_options.zoom = opt_options.zoom || [];
+        opt_options.zoomSpeed = typeof opt_options.zoomSpeed === "number" ? opt_options.zoomSpeed : -1;
 
         // Get the svg document
         var svg = self;
@@ -55,13 +108,13 @@
 
         // Create the rectangle
         var rect = new SVG(document.createDocumentFragment()).rect().attr({
-            width: svg.width(),
-            height: svg.height(),
-            fill: "none"
+            width: svg.width()
+          , height: svg.height()
+          , fill: "none"
         }).style("pointer-events", "all");
 
         // Insert the rectangle
-        self.parent.node.insertBefore(rect.node, self.node)
+        self.parent.node.insertBefore(rect.node, self.node);
 
         /*!
          * updateMatrix
@@ -73,11 +126,11 @@
          */
         function updateMatrix() {
             self.attr("transform", "matrix(" + [
-                pz.transform.scaleX,
-                0, 0,
-                pz.transform.scaleY,
-                pz.transform.x,
-                pz.transform.y
+                pz.transform.scaleX
+              , 0, 0
+              , pz.transform.scaleY
+              , pz.transform.x
+              , pz.transform.y
             ].join(",")+ ")");
         }
 
@@ -97,11 +150,32 @@
             var tr = pz.transform = self.transform();
             var diffX = pz.pan.fPos.x - pz.pan.iPos.x;
             var diffY = pz.pan.fPos.y - pz.pan.iPos.y;
-            tr.x += diffX;
-            tr.y += diffY;
-            pz.pan.iPos = pz.pan.fPos;
-            self.node.dispatchEvent(new CustomEvent("pan", e, tr));
-            updateMatrix();
+            pz.setPosition(tr.x + diffX, tr.y + diffY);
+            self.node.dispatchEvent(new CustomEvent("pan", { detail: { e: e, tr: tr } }));
+        }
+
+        /*!
+         * mousePos
+         * Returns the mouse point coordinates.
+         *
+         * @name mousePos
+         * @function
+         * @param {Event} e The mouse event.
+         * @param {Boolean} rel If `true`, the relative coordinates will be returned instead.
+         * @return {Object} An object containing the relative coordinates.
+         */
+        function mousePos(e, rel) {
+            var bbox = self.parent.node.getBoundingClientRect()
+              , abs = {
+                    x: e.clientX || e.touches[0].pageX
+                  , y: e.clientY || e.touches[0].pageY
+                }
+              ;
+            if (!rel) { return abs; }
+            return {
+                x: abs.x - bbox.left
+              , y: abs.y - bbox.top
+            };
         }
 
         /*!
@@ -113,29 +187,30 @@
          * @param {Event} e The internal listener event.
          * @return {undefined}
          */
-        function zoom (e) {
+        function doZoom (e) {
 
             // Get the relative mouse point
-            var rP = mousePos(e, true);
-            var oX = rP.x;
-            var oY = rP.y;
+            var rP = mousePos(e, true)
+              , oX = rP.x
+              , oY = rP.y
+              ;
 
             e.deltaY = e.deltaY || e.wheelDeltaY;
 
             // Compute the new scale
-            var d = e.deltaY / 1000;
-            var tr = pz.transform = self.transform();
-            var scale = tr.scaleX + (tr.scaleX * d);
+            var d = opt_options.zoomSpeed * e.deltaY / 1000
+              , tr = pz.transform = self.transform()
+              , scale = tr.scaleX + (tr.scaleX * d)
+              , scaleD = scale / tr.scaleX
 
-            var scaleD = scale / tr.scaleX;
+                // Get the current x, y
+              , currentX = tr.x
+              , currentY = tr.y
 
-            // Get the current x, y
-            var currentX = tr.x;
-            var currentY = tr.y;
-
-            // Compute the final x, y
-            var x = scaleD * (currentX - oX) + oX;
-            var y = scaleD * (currentY - oY) + oY;
+                // Compute the final x, y
+              , x = scaleD * (currentX - oX) + oX
+              , y = scaleD * (currentY - oY) + oY
+              ;
 
             // Handle zoom restrictions
             if (scale > opt_options.zoom[1]) {
@@ -153,34 +228,11 @@
             tr.x = x;
             tr.y = y;
 
-            self.node.dispatchEvent(new CustomEvent("zoom", e, tr));
+            self.node.dispatchEvent(new CustomEvent("zoom", { detail: { e: e, tr: tr } }));
             updateMatrix();
 
             // Prevent the default browser behavior
             e.preventDefault();
-        }
-
-        /*!
-         * mousePos
-         * Returns the mouse point coordinates.
-         *
-         * @name mousePos
-         * @function
-         * @param {Event} e The mouse event.
-         * @param {Boolean} rel If `true`, the relative coordinates will be returned instead.
-         * @return {Object} An object containing the relative coordinates.
-         */
-        function mousePos(e, rel) {
-            var bbox = self.bbox();
-            var abs = {
-                x: e.clientX || e.touches[0].pageX,
-                y: e.clientY || e.touches[0].pageY
-            };
-            if (!rel) { return abs; }
-            return {
-                x: abs.x - bbox.x,
-                y: abs.y - bbox.y
-            };
         }
 
         // The event listeners
@@ -188,25 +240,25 @@
             mouse_down: function (e) {
                 pz.pan.mousedown = true;
                 pz.pan.iPos = mousePos(e);
-            },
-            mouse_up: function (e) {
+            }
+          , mouse_up: function (e) {
                 pz.pan.mousedown = false;
                 pz.pan.fPos = mousePos(e);
                 pan();
-            },
-            mouse_move: function (e) {
+            }
+          , mouse_move: function (e) {
                 if (!pz.pan.mousedown) { return; }
                 pz.pan.fPos = mousePos(e);
                 pan();
-            },
-            mouse_leave: function (e) {
+            }
+          , mouse_leave: function (e) {
                 pz.pan.mousedown = false;
             }
         };
 
         // Add event listeners
         rect
-          .on(mousewheel, zoom)
+          .on(mousewheel, doZoom)
           .on("mousedown", EventListeners.mouse_down)
           .on("touchstart", EventListeners.mouse_down)
           .on("mousemove", EventListeners.mouse_move)
@@ -216,7 +268,7 @@
           .on("mouseleave", EventListeners.mouse_leave)
           ;
 
-        self.on(mousewheel, zoom);
+        self.on(mousewheel, doZoom);
         return pz;
     }
 
